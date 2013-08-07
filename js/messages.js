b5370038591b89d860c60478d7eb5d511030e90f
Thijs Van der Schaeghe <thijs@catlab.be>
2013-08-07 10:48:16 +0000

    Changing time calculations

--- js/messages.js
+++ js/messages.js
@@ -3,6 +3,46 @@ function getTimeSince (date)
  	var interval = Math.floor(seconds / 31536000);
  	var seconds = Math.floor((new Date() - date) / 1000);
 
+    if (seconds < 60 * 60 * 12)
+    {
+        interval = Math.floor(seconds / 3600);
+        if (interval >= 1) {
+            return interval + " hours";
+        }
+        interval = Math.floor(seconds / 60);
+        if (interval >= 1) {
+            return interval + " minutes";
+        }        
+
+        return Math.floor(seconds) + " seconds";
+    }
+    else
+    {
+        var y = (new Date()).getFullYear ();
+        if (y - date.getFullYear () > 0)
+        {
+            return y - date.getFullYear () + ' years';
+        }
+
+        var m = (new Date()).getMonth ();
+        if (m - date.getMonth () > 0)
+        {
+            return m - date.getMonth () + ' months';
+        }
+
+        var d = (new Date()).getDate ();
+        if (d - date.getDate () > 0)
+        {
+            return d - date.getDate () + ' days';
+        }
+
+        interval = Math.floor(seconds / 3600);
+        if (interval >= 1) {
+            return interval + " hours";
+        }
+    }
+
+    /*
     if (interval >= 1) {
         return interval + " years";
     }
@@ -14,15 +54,9 @@ function getTimeSince (date)
     if (interval >= 1) {
         return interval + " days";
     }
-    interval = Math.floor(seconds / 3600);
-    if (interval >= 1) {
-        return interval + " hours";
-    }
-    interval = Math.floor(seconds / 60);
-    if (interval >= 1) {
-        return interval + " minutes";
-    }
+
     return Math.floor(seconds) + " seconds";
+    */
 }
 
 function updateTimers ()
