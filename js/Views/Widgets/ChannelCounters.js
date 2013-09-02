7dcbff13b62cbb2ef876617a13728523f3d1eccc
Thijs Van der Schaeghe <thijs@catlab.be>
2013-09-02 15:04:59 +0000

    New dashboard (without layout)

---
+++ js/Views/Widgets/ChannelCounters.js
@@ -0,0 +1,15 @@
+/**
+* A standard widget
+*/
+Cloudwalkers.Views.Widgets.ChannelCounters = Cloudwalkers.Views.Widgets.Widget.extend({
+
+	'innerRender' : function (el)
+	{
+		el.html (Mustache.render (Templates.channelcounters, this.options.channel));
+
+		console.log (this.options.channel);
+
+		return this;
+	}
+
+});
