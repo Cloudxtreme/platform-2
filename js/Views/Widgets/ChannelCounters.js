5db23cb7d21e68efe916582c4cfb978d7a82dbbd
Thijs Van der Schaeghe <thijs@catlab.be>
2013-09-05 08:13:44 +0000

    Fixing channel counter

--- js/Views/Widgets/ChannelCounters.js
+++ js/Views/Widgets/ChannelCounters.js
@@ -6,9 +6,15 @@ Cloudwalkers.Views.Widgets.ChannelCounters = Cloudwalkers.Views.Widgets.Widget.e
 	'innerRender' : function (el)
 	{
 		var data = {};
-		jQuery.extend (data, this.options.channel);
+		jQuery.extend (true, data, this.options.channel);
 
 		data.message_count = data.unread;
+
+		for (var i = 0; i < data.streams.length; i ++)
+		{
+			data.streams[i].message_count = data.streams[i].unread;
+		}
+
 		el.html (Mustache.render (Templates.messagecounter, data));
 
 		return this;
