47a8b09ea9ef83694fec1cbc8bc5ec1dd3cb6589
Thijs Van der Schaeghe <thijs@catlab.be>
2013-10-23 09:40:44 +0000

    Trending also sorted on date to avoid message jumping.

--- js/Collections/Trending.js
+++ js/Collections/Trending.js
@@ -6,6 +6,11 @@ Cloudwalkers.Collections.Trending = Cloudwalkers.Collections.Channel.extend
 	{
 		this.comparator = function (message1, message2)
 		{
+			if (parseInt(message1.get ('engagement')) == parseInt(message2.get ('engagement')))
+			{
+				return message1.date ().getTime () < message2.date ().getTime () ? 1 : -1;
+			}
+
 			return parseInt(message1.get ('engagement')) < parseInt(message2.get ('engagement')) ? 1 : -1;
 		};
 	}
