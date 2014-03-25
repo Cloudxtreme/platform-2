f4f3032c672cfb1c121b723f98ded8ed3deaea83
Koen Betsens <koen@tick.ee>
2014-03-25 13:40:53 +0100

    Cloudwalkers 1.0 rc2 bugfixes

--- js/Cloudwalkers.js
+++ js/Cloudwalkers.js
@@ -279,20 +279,40 @@ Backbone.Collection = Backbone.Collection.extend({
 	{
 		// Ignore empty id lists
 		if(!ids) ids = [];
+
+		var list = [];
+		var fresh = _.compact( ids.map(function(id)
+		{
+			// In current Collection
+			var model = this.get(id);
+			
+			// Or in Session collection
+			if(!model)
+				model = Cloudwalkers.Session.user.account[this.typestring].get (id);
+			
+			// Or create new
+			if(!model) model = this.create({id: id});
+			else this.add(model);
+				
+			list.push(model);
+			
+			return id;
 		
+		}, this));
+				
 		// Get list based on ids
-		if(ids.length)
+		if(fresh.length)
 		{
 			this.endpoint = this.parentmodel? this.typestring: null;
-			this.parameters = {ids: ids.join(",")};
+			this.parameters = {ids: fresh.join(",")};
 			
 			this.fetch({remove: false});
 		}
 		
 		// Trigger listening models
-		this.trigger("seed", ids);
+		this.trigger("seed", list);
 
-		return ids;
+		return list;
 	},
 	
 	'more' : function(model, params)
