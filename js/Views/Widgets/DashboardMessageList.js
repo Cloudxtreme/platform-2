405d153c3b64719f192e6ed8223c6bae24065451
Rui Molefas <rmolefas@agap2.pt>
2014-05-08 16:14:42 +0100

    Added loading effect to the dashboard.

--- js/Views/Widgets/DashboardMessageList.js
+++ js/Views/Widgets/DashboardMessageList.js
@@ -22,7 +22,7 @@ Cloudwalkers.Views.Widgets.DashboardMessageList = Cloudwalkers.Views.Widgets.Mes
 		
 		// Listen to model
 		this.listenTo(this.model.messages, 'seed', this.fill);
-		
+		this.loadListeners(this.model.messages, ['request', 'sync', 'ready']);
 		
 		this.initializeWidget ();
 	},
@@ -31,7 +31,7 @@ Cloudwalkers.Views.Widgets.DashboardMessageList = Cloudwalkers.Views.Widgets.Mes
 	{
 		this.$el.html (Mustache.render (Templates.dashboardmessagecontainer, this.options));
 		this.$container = this.$el.find ('.messages-container');
-		
+		this.$loadercontainer = this.$el.find ('.portlet-body');
 		//this.type = (this.type == "news" || this.type == "profiles")? "trending": this.type;
 		
 		//if(!this.options.filters) this.options.filters = {};
