0de990dba4716dc94367ba75a0ff30d2c18c8fe4
Rui Molefas <rmolefas@agap2.pt>
2014-07-09 15:06:06 +0100

    Hot fix for image & excludes

--- js/Models/Message.js
+++ js/Models/Message.js
@@ -484,7 +484,7 @@ Cloudwalkers.Models.Message = Backbone.Model.extend({
 		var variation = this.getvariation(streamid);
 		var attachments = variation? variation.attachments : [];
 
-		if(_.isObject(image)){	// It's a default iamge
+		if(_.isNumber(image)){	// It's a default iamge
 			this.addexclude(streamid, image)			
 		}else{					// It's a variation image
 			for(n in attachments){
@@ -494,18 +494,18 @@ Cloudwalkers.Models.Message = Backbone.Model.extend({
 		}
 	},
 
-	'addexclude' : function(streamid, image){
+	'addexclude' : function(streamid, index){
 
 		var variation = this.getvariation(streamid) || this.setvariation(streamid);;
 		var excludes = variation.excludes;
 
 		if(variation.excludes)
-			variation.excludes.push(image);
+			variation.excludes.push(index);
 		else
-			variation.excludes = [image];
+			variation.excludes = [index];
 	},
 
-	'checkexclude' : function(streamid, image){
+	'checkexclude' : function(streamid, index){
 
 		var variation = this.getvariation(streamid);
 		var excludes;
@@ -515,7 +515,7 @@ Cloudwalkers.Models.Message = Backbone.Model.extend({
 
 		if(excludes){
 			for(n in excludes){
-				if(excludes[n].name == image.name)
+				if(excludes[n] == index)
 					return true;
 			}
 		}
