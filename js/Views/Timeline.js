35689f3bbf0f7d685eac63df38716fa9d15ee63c
Rui Molefas <rmolefas@agap2.pt>
2014-07-15 16:56:39 +0100

    Reverted Timeline changes

--- js/Views/Timeline.js
+++ js/Views/Timeline.js
@@ -71,8 +71,8 @@ Cloudwalkers.Views.Timeline = Cloudwalkers.Views.Pageview.extend({
 
 		// Add models to view
 		for (n in models)
-		{	console.log(models[n])
-			var view = new Cloudwalkers.Views.Entry ({model: models[n], template: 'newmessagetimeline', type: 'full', parameters:{trendview: this.trending}/*, parameters: this*/});
+		{	
+			var view = new Cloudwalkers.Views.Entry ({model: models[n], template: 'messagetimeline', type: 'full', parameters:{trendview: this.trending}/*, parameters: this*/});
 			
 			this.entries.push (view);
 			
