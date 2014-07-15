783a3711ebb3b91636d96d0df10980f65f47fe86
Rui Molefas <rmolefas@agap2.pt>
2014-07-15 16:12:17 +0100

    Canned model update

--- js/Models/CannedResponse.js
+++ js/Models/CannedResponse.js
@@ -1,15 +1,20 @@
 Cloudwalkers.Models.CannedResponse = Backbone.Model.extend({
 
+	'endpoint' : 'messages',
+
 	'initialize' : function ()
 	{
 		this.streams = Cloudwalkers.Session.getStreams('canned');
 	},
 
 	'url' : function()
-	{
-		var url = CONFIG_BASE_URL + 'json/accounts/' + Cloudwalkers.Session.getAccount ().id + '/messages';
-
-		return url;
+	{	
+		var url = [CONFIG_BASE_URL + 'json'];
+		
+		if(this.id)			url.push(this.endpoint + '/' + this.id)
+		else if(!this.id)	url.push('accounts/' + Cloudwalkers.Session.getAccount ().id + this.endpoint)
+		
+		return url.join("/");
 	},
 
 	'parse' : function(response)
