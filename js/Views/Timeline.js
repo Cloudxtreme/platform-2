27acaad82be40a0965d909918842f974b09cb821
Rui Molefas <rmolefas@agap2.pt>
2014-08-06 17:15:28 +0100

    Hotfix: Timeline actions button removed when there are no actions

--- js/Views/Timeline.js
+++ js/Views/Timeline.js
@@ -74,7 +74,7 @@ Cloudwalkers.Views.Timeline = Cloudwalkers.Views.Pageview.extend({
 
 		// Add models to view
 		for (n in models)
-		{
+		{	
 			var view = new Cloudwalkers.Views.Entry ({model: models[n], template: 'messagetimeline', type: 'full', parameters:{trendview: this.trending}/*, parameters: this*/});
 			
 			this.entries.push (view);
