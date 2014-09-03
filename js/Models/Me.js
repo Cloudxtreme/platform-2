5b598b6e806ee175588546559d868a6a269c6560
Rui Molefas <rmolefas@agap2.pt>
2014-09-03 19:29:58 +0100

    debug

--- js/Models/Me.js
+++ js/Models/Me.js
@@ -16,13 +16,6 @@ Cloudwalkers.Models.Me = Cloudwalkers.Models.User.extend({
 		var param = this.endpoint?
 			this.endpoint :
 			(Store.exists("me")? "?include_accounts=ids": "");
-		console.log(Cloudwalkers.Session.api + '/user/me' + param)
-
-		var req = new XMLHttpRequest();
-		req.open('GET', 'https://stagingapi.cloudwalkers.be/1/user/me' , false);
-		req.send(null);
-		var headers = req.getAllResponseHeaders().toLowerCase();
-		console.log(headers);
 
 		return Cloudwalkers.Session.api + '/user/me' + param;
 	},
