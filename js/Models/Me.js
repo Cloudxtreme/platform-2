5023ec05a0fde8a5819710af8ada407ef155bdfe
Rui Molefas <rmolefas@agap2.pt>
2014-09-04 14:50:10 +0100

    Hotfix stable

--- js/Models/Me.js
+++ js/Models/Me.js
@@ -188,7 +188,7 @@ Cloudwalkers.Models.Me = Cloudwalkers.Models.User.extend({
 	'offline' : function ()
 	{
 		Cloudwalkers.Session.reset();
-		//window.location = "/login.html";
+		window.location = "/login.html";
 		
 		// If Me exists local, use when offline.
 		// if (Store.exists("me")) this.trigger("change");
