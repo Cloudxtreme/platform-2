6b94324b972dba9e7cd1e2fea30d85b26c857dbe
Koen Betsens <koen@tick.ee>
2014-09-04 12:38:47 +0200

    ie fix update

--- js/ie-fix.js
+++ js/ie-fix.js
@@ -1,15 +1,21 @@
-/*
- *	Add authorization headers to each Backbone.sync call
- */
-Backbone.ajax = function()
+$( function()
 {
-	// Is there a auth token?
-	if(Cloudwalkers.Session.authenticationtoken)
-		
-		arguments[0].headers = {
-            'Authorization': 'Bearer ' + Cloudwalkers.Session.authenticationtoken,
-            'Accept': "application/json"
-        };
 
-	return Backbone.$.ajax.apply(Backbone.$, arguments);
-};
+	/*
+	 *	Add authorization headers to each Backbone.sync call
+	 */
+	Backbone.ajax = function()
+	{
+		// Is there a auth token?
+		if(Cloudwalkers.Session.authenticationtoken)
+			
+			arguments[0].headers = {
+	            'Authorization': 'Bearer ' + Cloudwalkers.Session.authenticationtoken,
+	            'Accept': "application/json"
+	        };
+	
+		return Backbone.$.ajax.apply(Backbone.$, arguments);
+	};
+
+
+});
