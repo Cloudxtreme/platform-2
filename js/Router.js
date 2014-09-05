1b2251fc6a37cc100e18dbf74b8b2087e94c85a7
Rui Molefas <rmolefas@agap2.pt>
2014-09-05 16:02:00 +0100

    Removed disconnect on account change

--- js/Router.js
+++ js/Router.js
@@ -86,7 +86,7 @@ Cloudwalkers.Router = Backbone.Router.extend ({
 	{	
 		if(accountid != Cloudwalkers.Session.get("currentAccount"))
 		{
-			Cloudwalkers.Session.updateSetting("currentAccount", accountid, {success: this.home}); //patch: true
+			Cloudwalkers.Session.updateSetting("currentAccount", accountid, {success: this.home.bind(this, true)}); //patch: true
 		}
 	},
 	
@@ -373,20 +373,25 @@ Cloudwalkers.Router = Backbone.Router.extend ({
 		Cloudwalkers.RootView.setView (new Cloudwalkers.Views.Resync({returnto: view}));
 	},
 
-	'home' : function ()
+	'home' : function (changeaccount)
 	{	
-		$.ajax({ url: config.authurl + "revoke", headers : {
-            'Authorization': 'Bearer ' + Cloudwalkers.Session.authenticationtoken,
-            'Accept': "application/json"
-        },
-        success: function()
-        {
-        	window.location = "/";
-        }});
+		if(!changeaccount){
+
+			$.ajax({ url: config.authurl + "revoke", headers : {
+	            'Authorization': 'Bearer ' + Cloudwalkers.Session.authenticationtoken,
+	            'Accept': "application/json"
+	        },
+	        success: function()
+	        {
+	        	window.location = "/";
+	        }});
+		}else{
+			window.location = "/";
+		}
 		
 		Cloudwalkers.RootView.view.remove();
 		Cloudwalkers.RootView.navigation.remove();
-		Cloudwalkers.Session.reset();
+		Cloudwalkers.Session.reset(changeaccount);
 		
 		return false;
 	},
