6799217b3d4dfb12861bac770297b9617142732c
Rui Molefas <rmolefas@agap2.pt>
2014-09-22 09:59:07 +0100

    Temporarily removed composes's "thankyou" for testing purposes.

--- js/Views/Compose.js
+++ js/Views/Compose.js
@@ -1584,7 +1584,7 @@ Cloudwalkers.Views.Compose = Backbone.View.extend({
 		} 
 		
 		// Or just post
-		else this.draft.save({status: "scheduled"}, {success: this.thankyou.bind(this, 'post'), error: this.oops.bind(this, 'post')});
+		else this.draft.save({status: "scheduled"}, {/*success: this.thankyou.bind(this, 'post'),*/ error: this.oops.bind(this, 'post')});
 		
 		this.loadingalert();
 
