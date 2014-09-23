26212a62e17c4184b36b53a5b4a5afe67e017b5f
Rui Molefas <rmolefas@agap2.pt>
2014-09-23 10:12:54 +0100

    Updated counters
    Conflicts:
    	js/Views/Widgets/MessagesCounters.js

--- js/Views/Widgets/MessagesCounters.js
+++ js/Views/Widgets/MessagesCounters.js
@@ -15,32 +15,11 @@ Cloudwalkers.Views.Widgets.MessagesCounters = Cloudwalkers.Views.Widgets.Widget.
 	'initialize' : function(options)
 	{
 	
-		if(!options.color) this.options.color = this.color;
+		 if(!options.color) this.options.color = this.color;
 		
 		// The list source is either the streams or subchannels
 		this.list = options.channel[options.source];
-		this.listenToOnce(options.channel, 'sync', this.updatedcounters.bind(this))
-
-		options.channel.fetch();
-		this.rendercounters();
-	},
-
-	'updatedcounters' : function(models)
-	{	
-		this.options.channel = models;
-		this.options.channel[this.options.source].models = this.options.channel.get(this.options.source);
 		
-		for(n in this.options.channel[this.options.source].models){
-			this.options.channel[this.options.source].models[n] = new Cloudwalkers.Models.Stream(this.options.channel[this.options.source].models[n]);
-		}
-		
-		this.list = this.options.channel[this.options.source];
-		
-		this.rendercounters();
-	},
-
-	'rendercounters' : function()
-	{	
 		if(this.list)
 		for (n in this.list.models)
 		{
@@ -60,11 +39,11 @@ Cloudwalkers.Views.Widgets.MessagesCounters = Cloudwalkers.Views.Widgets.Widget.
 
 				var counter;
 
-				if(this.options.source == "outgoing")
-					counter = this.list.models[n].get("counters").total.scheduled.messages.total;
+				if(options.source == "outgoing")
+					this.list.models[n].count = this.list.models[n].get("counters").total.scheduled.messages.total;
 				else
-					counter = this.list.models[n].get("counters").recent.incoming.any.unread;
-
+					this.list.models[n].count = this.list.models[n].get("counters").recent.incoming.any.unread;
+				
 			} else if(this.list.models[n].channels){
 				// Keyword Filters
 				
@@ -90,16 +69,14 @@ Cloudwalkers.Views.Widgets.MessagesCounters = Cloudwalkers.Views.Widgets.Widget.
 				}
 
 			}
-			
-			if(!this.list.models[n].count)
-				this.list.models[n].count = this.list.models[n].get("count")[this.options.countString];
-		}
 
-		this.trigger('view:update');
+			if(!this.list.models[n].count)
+				this.list.models[n].count = this.list.models[n].get("count")[options.countString];
+		}
 	},
 	
 	'render' : function ()
-	{	
+	{
 		var data = { list: [] };		
 		$.extend (data, this.options);
 		
