114576bc6c24bb965d0c2ba898ce07f05fadc17f
Rui Molefas <rmolefas@agap2.pt>
2014-10-07 21:26:52 +0100

    Readded config

---
+++ src/js/config.js
@@ -0,0 +1,53 @@
+define({
+
+	/* Insert your appid below. 
+	 * If you dont have one, register @ https://devapi.cloudwalkers.be/oauth2/register
+	 */
+	appid : "--appid--", 
+	apiurl: "https://devapi.cloudwalkers.be/",
+	authurl: "https://devapi.cloudwalkers.be/oauth2/",
+	
+	setloginwindow : function ()
+	{
+		$("iframe").get(0).src = this.authurl + "authorize?response_type=token&state=xyz&client_id=" + this.appid + "&redirect_uri=" + origin() + "/auth.html";
+	},
+	
+	hello : function ()
+	{
+		window.location = "/";
+	},
+	
+	hasToken : function ()
+	{
+		// Authentication
+		var token = window.localStorage.getItem('token');
+		
+		if(token && token.length > 9) this.hello();
+		else	
+		{
+			if(token) window.localStorage.removeItem('token');
+			
+			this.setloginwindow();
+			window.addEventListener("message", this.receiveToken.bind(this), false);	
+		}
+	},
+	
+	receiveToken :function (event)
+	{
+		if (event.origin !== origin())
+		return;
+		
+		if (event.data && event.data.length > 9)
+		{
+			window.localStorage.setItem('token', event.data);
+			this.hello();
+		}
+		else this.setloginwindow();
+	}
+});
+
+var origin = function ()
+{
+	return (window.location.origin)? window.location.origin : window.location.protocol + "//" + window.location.hostname;
+}
+		
